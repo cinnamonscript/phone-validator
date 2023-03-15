57bb66aeba9e1d3c0bada698b37bf6dd35080c03
cinnamonscript <94289131+cinnamonscript@users.noreply.github.com>
2023-03-15 20:24:14 +1000

    Organised files into /src
    Organised files into /src

---
+++ src/Models/PhoneNumberDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhoneValidatorAPI.Models
+{
+    public class PhoneNumberDetails
+    {
+        public bool IsValid { get; set; }
+        public bool IsPossible { get; set; }
+        public string PhoneType { get; set; }
+        public string InternationalFormat { get; set; }
+
+
+    }
+}
